f187d3ded11d78a0a48a8abde34d49205352838a
Andreas Salvermoser <andreas@yahoo.com>
2023-03-23 19:54:42 -0300

    try

--- functions/sendinblue.js
+++ functions/sendinblue.js
@@ -1,31 +1,36 @@
-async function createSendinblueUser(email, firstName, lastName) {
-  try {
-    // Define the payload for the API request, using the user information extracted from the Firestore doc
-    const payload = {
-      email: email,
-      attributes: {
-        FIRSTNAME: firstName,
-        LASTNAME: lastName
-      }
-    };
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const fetch = require('node-fetch');
 
-    // Send the API request to create a new user in your email marketing campaign list
-    const response = await fetch('https://api.sendinblue.com/v3/contacts', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'api-key': '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'
-      },
-      body: JSON.stringify(payload)
-    });
+admin.initializeApp();
 
-    // Check if the API request was successful
-    if (response.status === 201) {
-      console.log('User added to email marketing campaign list.');
-    } else {
-      console.log('Failed to add user to email marketing campaign list.');
+exports.createContactInSendinblue = functions.firestore
+  .document('users/{userId}')
+  .onCreate(async (snap, context) => {
+    const data = snap.data();
+    const name = data.user_firstname;
+    const lastname = data.user_lastname;
+    const email = data._useremail;
+
+    try {
+      const response = await fetch('https://api.sendinblue.com/v3/contacts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'api-key': '1ffcea85ac55ee30cd4b41e2c1dc91d1bdfb458339ff44b45367a9dd6718fcd6-xtF8KJWVLQEhk2n7'
+        },
+        body: JSON.stringify({
+          email: email,
+          attributes: {
+            FIRSTNAME: name,
+            LASTNAME: lastname
+          }
+        })
+      });
+
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+    } catch (error) {
+      console.error(error);
     }
-  } catch (error) {
-    console.error(error);
-  }
-}
+  });
