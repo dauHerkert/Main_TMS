584372fa68a8ebbb7a810e7ec2ee15857b1e76ea
Andreas Salvermoser <asalvermoser@taoti.com>
2023-07-14 00:06:58 -0500

    jquery test

--- components/base2.js
+++ components/base2.js
@@ -1,29 +1,6 @@
-
-
-    console.log('------------------------------------------------------------------------------------------------------');
-    console.log('Starting firebase2 config');
-    console.log('------------------------------------------------------------------------------------------------------');
-
-    // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAMoVGExyvb89NEa-M3EMl647-_rUN4XP4",
-  authDomain: "bho-copy.firebaseapp.com",
-  projectId: "bho-copy",
-  storageBucket: "bho-copy.appspot.com",
-  messagingSenderId: "792136477405",
-  appId: "1:792136477405:web:001c2802b72baa4321c934"
-};
-
-/*
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth(app);
-    const db = getFirestore(app);
-    const storage = getStorage(app);
-    const user = auth.currentUser;
-    let currentLang = 'en';*/
-
-    console.log('config', firebaseConfig);
+jQuery(function() {
+  console.log('testing with jQuery');
+});
 
 
 
