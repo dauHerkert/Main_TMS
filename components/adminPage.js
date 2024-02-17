2a77b4833e3bae19dcde4eac4cca20caf4651ce5
niklasdiet <niki.di@web.de>
2024-02-17 17:22:19 +0100

    Aktualisiere Konsolenausgaben in adminPage.js

--- components/adminPage.js
+++ components/adminPage.js
@@ -1121,7 +1121,7 @@ export async function pageAdmin(user) {
           console.log('admin_user_name.value >>>>>> ', admin_user_name.value);
           console.log('press_user_title.textContent >>>>>> ', press_user_title.textContent);
           console.log('>>>>>>>>>>>>>>>>>> ',user_type);
-          console.log('>>>>>>>>>>>>>>>>>> ',userData.data().press_form_user);
+          console.log('>>>>>>>>>>>>>>>>>> ${userData.data().press_form_user}');
           
           if (userData.data().press_form_user) {
             if (user_status_update.value == 'Declined') {
