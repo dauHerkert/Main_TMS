29c218acbd22b2e4dd9ddfef0044edc06ca19fba
Niklas <30591262+niklasdiet@users.noreply.github.com>
2024-03-11 12:00:51 +0100

    Update a_firebaseConfig.js

--- components/a_firebaseConfig.js
+++ components/a_firebaseConfig.js
@@ -5,12 +5,12 @@ import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from 'fire
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAMoVGExyvb89NEa-M3EMl647-_rUN4XP4", 
-  authDomain: "bho-copy.firebaseapp.com",
-  projectId: "bho-copy",
-  storageBucket: "bho-copy.appspot.com",
-  messagingSenderId: "792136477405",
-  appId: "1:792136477405:web:001c2802b72baa4321c934"
+  apiKey: "AIzaSyDuWqqoSEfawmsdJNQzIDKk6lfvEKAubmA",
+  authDomain: "porsche-tms.firebaseapp.com",
+  projectId: "porsche-tms",
+  storageBucket: "porsche-tms.appspot.com",
+  messagingSenderId: "267742806983",
+  appId: "1:267742806983:web:7e8a7ed147f052676b8fe2"
 };
 
 // Initialize Firebase
